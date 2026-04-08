d3f4ae82ee769fba88392de4dfae00a283f0ed68
Paul Irish <commits@paul.irish>
2026-04-08 15:54:19 -0700

    refactor: remove unused LooseEvalsReport

--- eval-view/evals.d.ts
+++ eval-view/evals.d.ts
@@ -1,9 +1,6 @@
 import type { EvalsReport } from '../harness/lib/metrics.ts';
 
-export interface LooseEvalsReport extends Partial<Omit<EvalsReport, 'results'>> {
-  results?: Record<string, any>;
-  [key: string]: any;
-}
+
 
 declare global {
   interface Window {
