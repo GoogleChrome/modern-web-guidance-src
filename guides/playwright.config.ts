336384d427f96da028787b047de7a4959b26a7b1
Micah Jo <micahjo@google.com>
2026-04-09 10:42:06 -0400

    update paths in playwright config

--- guides/playwright.config.ts
+++ guides/playwright.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig, devices } from '@playwright/test';
-import * as path from 'path';
+import { fileURLToPath } from 'node:url';
+import * as path from 'node:path';
 import * as os from 'os';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   timeout: 10000,
