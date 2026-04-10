bfefd8af5568853b0e6d4154e7fad4eb5c2ff811
Paul Irish <commits@paul.irish>
2026-04-10 11:19:40 -0700

    ya

--- serving/lib/dummy-onnx.ts
+++ serving/lib/dummy-onnx.ts
@@ -1,6 +1,4 @@
 export default new Proxy({}, {
-  get: function(target, name) {
-    // Return a dummy function for everything to satisfy instanceof checks
-    return function() {};
-  }
+  // Return a dummy function for everything to satisfy instanceof checks
+  get: () => () => {}
 });
