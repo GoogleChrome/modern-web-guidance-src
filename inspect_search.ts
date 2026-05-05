b3ceb34839ea3c05e1be6b110a4f0e87605eed05
Paul Irish <commits@paul.irish>
2026-05-05 15:15:33 -0700

    drop

--- inspect_search.ts
+++
@@ -1,9 +0,0 @@
-import { searchUseCases } from "./serving/lib/search.ts";
-
-async function run() {
-  const query = "autofill address form";
-  const results = await searchUseCases(query, 10);
-  console.log(JSON.stringify(results, null, 2));
-}
-
-run().catch(console.error);
